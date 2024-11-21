3715c3f0e01e16f5fdd9f2d07a0775648ce375a1
mininim <minsoplee2015@gmail.com>
2024-11-21 21:57:31 +0900

    feat: RecordButton UI 구현

---
+++ frontend/src/components/CreateDraft/RecordButton.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from "react";
+import recordIcon from "../../assets/icon/record-icon.svg";
+
+function RecordButton() {
+  return (
+    <button
+      className="cd-draftDropBox-button"
+      style={{ width: "118px", gap: "8px" }}
+    >
+      <img src={recordIcon} alt="Record Icon" />
+      <span>녹음</span>
+    </button>
+  );
+}
+
+export default RecordButton;
