684c3db04f1d07415f4c4df95610f34f0760f873
mininim <minsoplee2015@gmail.com>
2024-11-29 01:44:33 +0900

    feat: fetch RoleModel #17

--- frontend/src/components/ProjectPage/SelectRoleModel.jsx
+++ frontend/src/components/ProjectPage/SelectRoleModel.jsx
@@ -1,12 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/CreateProject.css";
 import DraftDropBox from "../CreateDraft/DraftDropBox";
 
 function SelectRoleModel() {
   const [file, setFile] = useState(null);
-  const [rolemodelList, setRoleModelList] = useState([
-    "김민지 아나운서 (8시 뉴스)",
-  ]);
+  const [rolemodelList, setRoleModelList] = useState([]);
+
+  useEffect(() => {
+    const fetchPractices = async () => {
+      try {
+        const response = await fetch("/api/v1/models");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        // API 데이터 매핑
+        const mappedData = data.map((item) => ({
+          id: item.id,
+          name: item.name,
+          type: item.type,
+          userid: item.userid,
+          duration: item.duration,
+          analysis: {
+            wpm: item.analysis?.wpm || 0,
+            pitch: item.analysis?.pitch || 0,
+            rest: item.analysis?.rest || 0,
+            energy: item.analysis?.energy || 0,
+            confidence: item.analysis?.confidence || 0,
+          },
+        }));
+
+        setRoleModelList(mappedData); // 상태 업데이트
+        console.log(data);
+        console.log(mappedData);
+      } catch (err) {
+        console.error(err);
+      } finally {
+      }
+    };
+    fetchPractices();
+  }, []);
 
   function CreateRoleModel() {
     return (
@@ -30,8 +64,16 @@ function SelectRoleModel() {
       <div className="cp-selectRoleModel">
         <h4>롤모델 둘러보기</h4>
         <div className="cp-selectRoleModel-scroll-container">
-          {rolemodelList.map((rolemodel, index) => (
-            <RoleModelCard index={index} title={rolemodel} />
+          {rolemodelList.map((rolemodel) => (
+            <RoleModelCard
+              key={rolemodel.id}
+              index={rolemodel.id}
+              title={rolemodel.name}
+              type={rolemodel.type}
+              userid={rolemodel.userid}
+              duration={rolemodel.duration}
+              analysis={rolemodel.analysis}
+            />
           ))}
         </div>
       </div>
@@ -49,4 +91,5 @@ function SelectRoleModel() {
     </div>
   );
 }
+
 export default SelectRoleModel;
