e7a78803a08e09624e9a77a68605d7a0a19614d2
mininim <minsoplee2015@gmail.com>
2024-11-29 12:25:32 +0900

    feat: 롤모델 카드 눌러서 롤모델 설정 구현 #17

--- frontend/src/components/ProjectPage/SelectRoleModel.jsx
+++ frontend/src/components/ProjectPage/SelectRoleModel.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import "../../styles/CreateProject.css";
 import DraftDropBox from "../CreateDraft/DraftDropBox";
 
-function SelectRoleModel() {
+function SelectRoleModel({}) {
+  const navigate = useNavigate();
   const [file, setFile] = useState(null);
   const [rolemodelList, setRoleModelList] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const { projectId } = useParams();
 
   useEffect(() => {
     const fetchPractices = async () => {
@@ -42,7 +46,44 @@ function SelectRoleModel() {
     fetchPractices();
   }, []);
 
-  function CreateRoleModel() {
+  const hModalOpen = () => {
+    setIsOpen(true);
+  };
+  const hCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  const hCardClick = async (roleModelId) => {
+    try {
+      const response = await fetch(`/api/v1/projects`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          projectid: projectId,
+          name: "",
+          modeilid: roleModelId,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      console.log("API Response:", data);
+      // 강제 새로고침
+      // CreateProject에서 isSelected 관련 State 추가하기
+      window.location.reload();
+    } catch (error) {
+      console.error("Error sending API request:", error);
+      alert("Failed to select role model.");
+    }
+  };
+
+  function CreateRoleModel({ file, setFile }) {
     return (
       <div className="cp-createRoleModel">
         <h4>롤모델 생성하기</h4>
@@ -51,14 +92,24 @@ function SelectRoleModel() {
     );
   }
 
-  function RoleModelCard({ index, title }) {
+  function RoleModelCard({ index, title, onClick }) {
     return (
-      <div key={index} className="cp-rolemodel-card">
+      <div key={index} className="cp-rolemodel-card" onClick={onClick}>
         <div>{title}</div>
       </div>
     );
   }
-
+  function SelectRoleModelButton() {
+    return (
+      <button
+        className="cp-button"
+        onClick={() => {}}
+        disabled={false || !file}
+      >
+        {false ? "업로드 중..." : "생성 하기"}
+      </button>
+    );
+  }
   function SelectRoleModel() {
     return (
       <div className="cp-selectRoleModel">
@@ -66,6 +117,7 @@ function SelectRoleModel() {
         <div className="cp-selectRoleModel-scroll-container">
           {rolemodelList.map((rolemodel) => (
             <RoleModelCard
+              className=".cp-rolemodel-card"
               key={rolemodel.id}
               index={rolemodel.id}
               title={rolemodel.name}
@@ -73,6 +125,7 @@ function SelectRoleModel() {
               userid={rolemodel.userid}
               duration={rolemodel.duration}
               analysis={rolemodel.analysis}
+              onClick={() => hCardClick(rolemodel.id)}
             />
           ))}
         </div>
@@ -85,8 +138,14 @@ function SelectRoleModel() {
       <div className="cp-rolemodel-title">롤모델</div>
       <div className="cp-rolemodel-content">
         <h3>롤모델을 추가하여 프로젝트를 활성화해주세요</h3>
-        <CreateRoleModel />
-        <SelectRoleModel />
+        <CreateRoleModel file={file} setFile={setFile} />
+        {!file ? (
+          <SelectRoleModel />
+        ) : (
+          <div className="cp-button-container">
+            <SelectRoleModelButton />
+          </div>
+        )}
       </div>
     </div>
   );
